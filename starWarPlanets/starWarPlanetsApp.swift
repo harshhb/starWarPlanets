c460c636c4ac2eb3b3ca52e88221fc510be4e556
harshana.ekanayake <harshana@ideahub.lk>
2025-10-29 21:39:45 +0530

    Initial Commit

---
+++ starWarPlanets/starWarPlanetsApp.swift
@@ -0,0 +1,21 @@
+//
+//  starWarPlanetsApp.swift
+//  starWarPlanets
+//
+//  Created by Harshana Ekanayake on 2025-10-29.
+//
+
+import SwiftUI
+import CoreData
+
+@main
+struct starWarPlanetsApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
---
+++ starWarPlanetsTests/starWarPlanetsTests.swift
@@ -0,0 +1,17 @@
+//
+//  starWarPlanetsTests.swift
+//  starWarPlanetsTests
+//
+//  Created by Harshana Ekanayake on 2025-10-29.
+//
+
+import Testing
+@testable import starWarPlanets
+
+struct starWarPlanetsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
